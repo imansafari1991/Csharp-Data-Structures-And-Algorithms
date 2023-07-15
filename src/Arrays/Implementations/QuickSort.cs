054eeb8475d19a3cee60596d7e0f7c40fe606689
imansafari1991 <imansafari33@gmail.com>
2023-07-16 00:16:48 +0330

    add quick sort

---
+++ src/Arrays/Implementations/QuickSort.cs
@@ -0,0 +1,51 @@
+﻿using Arrays.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays.Implementations
+{
+    public class QuickSort<T> : IArraySorter<T> where T : IComparable
+    {
+        public T[] AscendingSort(T[] array)
+        {
+          return  Sort(array, 0, array.Length - 1);
+        }
+
+        public T[] DescendingSort(T[] array)
+        {
+            throw new NotImplementedException();
+        }
+        private static T[] Sort<T>(T[] array, int lower, int upper)
+ where T : IComparable
+        {
+            if (lower < upper)
+            {
+                int p = Partition(array, lower, upper);
+                Sort(array, lower, p);
+                Sort(array, p + 1, upper);
+            }
+            return array;
+        }
+        private static int Partition<T>(T[] array, int lower, int upper)
+ where T : IComparable
+        {
+            int i = lower;
+            int j = upper;
+            T pivot = array[lower];
+            // or: T pivot = array[(lower + upper) / 2];
+            do
+            {
+                while (array[i].CompareTo(pivot) < 0) { i++; }
+                while (array[j].CompareTo(pivot) > 0) { j--; }
+                if (i >= j) { break; }
+               Utilities.Swap(array, i, j);
+            }
+            while (i <= j);
+            return j;
+        }
+
+    }
+}
--- src/Arrays/Program.cs
+++ src/Arrays/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Arrays.Contracts;
+using Arrays.Implementations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,9 @@ namespace Arrays
     {
         public static void Main(string[] args)
         {
-
+            IArraySorter<int> arraySorter=new QuickSort<int>();
+            var arr = new[] { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+            arraySorter.AscendingSort(arr);
         }
     }
 }
